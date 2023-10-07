71016e5bd9d0a346325bf53aa572b4f815a9ed17
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2023-10-07 15:28:24 +0700

    Added 'add colour' button

--- App.js
+++ App.js
@@ -1,31 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
-import BlockRGB from "./BlockRGB";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View, Pressable } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BlockRGB from "./BlockRGB";
 
-var data = [];
-const Stack = createNativeStackNavigator();
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 128, green: 0, blue: 0, id: 0 },
+    { red: 0, green: 100, blue: 0, id: 1 },
+    { red: 0, green: 0, blue: 255, id: 2 },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.random() * 255,
+        green: Math.random() * 255,
+        blue: Math.random() * 255,
+        id: colorArray.length,
+      },
+    ]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={addColor}>
+        <Text> Add new color</Text>
+      </Pressable>
+      <FlatList
+        style={{ width: "100%" }}
+        data={colorArray}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function Home() {
-  return (
-    <View>
-      <View>
-        <Button>Add colour</Button>
-      </View>
-      <View></View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
